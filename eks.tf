3b62523af153515eff46410617beae2b234a6596
Eduardo Santos <edu.nascimento22@outlook.com>
2026-09-02 22:55:53 -0300

    cluster permissoes e deploy eks

--- eks.tf
+++ eks.tf
@@ -15,11 +15,32 @@ resource "aws_eks_cluster" "main" {
     ]
   }
 
-  # Ensure that IAM Role permissions are created before and deleted
-  # after EKS Cluster handling. Otherwise, EKS will not be able to
-  # properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
+}
+
+resource "aws_eks_access_entry" "eduardo" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = "arn:aws:iam::477478162709:user/eduardo"
+
+  depends_on = [
+    aws_eks_cluster.main
+  ]
+}
+
+resource "aws_eks_access_policy_association" "eduardo_admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = aws_eks_access_entry.eduardo.principal_arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [
+    aws_eks_access_entry.eduardo
   ]
 }
 
@@ -37,7 +58,7 @@ resource "aws_iam_role" "cluster" {
         Principal = {
           Service = "eks.amazonaws.com"
         }
-      },
+      }
     ]
   })
 }
@@ -45,4 +66,57 @@ resource "aws_iam_role" "cluster" {
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster.name
+}
+
+resource "aws_iam_role" "node" {
+  name = "eks-node-group"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "main-ng"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = [aws_subnet.sub_a.id, aws_subnet.sub_b.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 1
+  }
+
+  instance_types = ["t3.small"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly
+  ]
 }
--- vpc.tf
+++ vpc.tf
@@ -7,9 +7,10 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "sub_a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_a_cidr
-  availability_zone = var.aws_region_az_a
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_a_cidr
+  availability_zone       = var.aws_region_az_a
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "sub_a"
@@ -17,9 +18,10 @@ resource "aws_subnet" "sub_a" {
 }
 
 resource "aws_subnet" "sub_b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_b_cidr
-  availability_zone = var.aws_region_az_b
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_b_cidr
+  availability_zone       = var.aws_region_az_b
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "sub_b"
