cfd04a85f8d3d958ccde893e22ec03ff3c9f6f92
Eduardo Santos <edu.nascimento22@outlook.com>
2026-09-04 00:52:20 -0300

    feat: aplicando sg liberando somente a rede interna

--- security-groups.tf
+++ security-groups.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "rds" {
   name        = "rds"
   vpc_id      = aws_vpc.main.id
 
-  tags = {
     Name = "rds"
+  tags = {
   }
 }
 
@@ -21,9 +21,30 @@ resource "aws_vpc_security_group_ingress_rule" "eks" {
   security_group_id            = aws_security_group.eks.id
   cidr_ipv4 = var.cidr_block
 
-  from_port                    = 80
-  to_port                      = 80
-  ip_protocol                  = "tcp"
+  ip_protocol = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "eks" {
+  security_group_id            = aws_security_group.eks.id
+  cidr_ipv4 = var.cidr_block
+
+  ip_protocol = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_app_my_ip" {
+  security_group_id = aws_security_group.eks.id
+  cidr_ipv4         = "179.98.123.83/32"
+  from_port         = 8080
+  to_port           = 8080
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "swagger_my_ip" {
+  security_group_id = aws_security_group.eks.id
+  cidr_ipv4         = "179.98.123.83/32"
+  from_port         = 8082
+  to_port           = 8082
+  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_mysql" {
